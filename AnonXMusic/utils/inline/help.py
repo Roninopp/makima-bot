25a384d9daec1146b7e333c64db33280f99bba70
VIVEK <99870677+Roninopp@users.noreply.github.com>
2026-05-19 22:18:01 +0530

    Update help.py

--- AnonXMusic/utils/inline/help.py
+++ AnonXMusic/utils/inline/help.py
@@ -10,7 +10,6 @@ def help_pannel(_, is_sudo, START: Union[bool, int] = None):
     second = [
         InlineKeyboardButton(
             text=_["BACK_BUTTON"],
-            # 👑 FIXED: Changed from settingsback_helper to our custom clean back route
             callback_data=f"settings_back_helper",
         ),
     ]
@@ -92,8 +91,8 @@ def help_pannel(_, is_sudo, START: Union[bool, int] = None):
         upl.append(
             [
                 InlineKeyboardButton(
-                    text=\"Ai/TTS/IMAGE Settings\",
-                    callback_data=\"help_callback hb16\",
+                    text="Ai/TTS/IMAGE Settings",
+                    callback_data="help_callback hb16",
                 )
             ]
         )

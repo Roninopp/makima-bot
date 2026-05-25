c88895bb82152dc84bf5ec7ebc59968ddb85682d
VIVEK <99870677+Roninopp@users.noreply.github.com>
2026-05-25 20:42:23 +0530

    Update welcome.py

--- AnonXMusic/plugins/tools/welcome.py
+++ AnonXMusic/plugins/tools/welcome.py
@@ -16,7 +16,7 @@ print("✅ WELCOME.PY: Loaded successfully! URL Mode Active.")
 
 # 🚨 PASTE YOUR GITHUB RAW LINK HERE 🚨
 # 🚨 PASTE YOUR POSTIMAGES DIRECT LINK HERE 🚨
-BACKGROUND_URL = "https://i.postimg.cc/4Nk6RrMc/background.png""
+BACKGROUND_URL = "https://i.postimg.cc/4Nk6RrMc/background.png"
 
 def circle_crop(image):
     mask = Image.new('L', image.size, 0)

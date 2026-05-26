33c3eb71c0896fc19d224d4082c4406ba0c95a6f
VIVEK <99870677+Roninopp@users.noreply.github.com>
2026-05-27 00:05:51 +0530

    Update welcome.py

--- AnonXMusic/plugins/tools/welcome.py
+++ AnonXMusic/plugins/tools/welcome.py
@@ -34,8 +34,8 @@ def circle_crop(image):
 async def auto_delete_welcome(client, chat_id, message_id):
     print(f"⏱️ WELCOME DEBUG: Countdown started! Will delete message {message_id} in 10 minutes.")
     
-    # 600 seconds = 5 minutes
-    await asyncio.sleep(300)
+    # 600 seconds = 1 minutes
+    await asyncio.sleep(55)
     
     print(f"🗑️ WELCOME DEBUG: 10 minutes passed! Attempting to delete message {message_id} in chat {chat_id}...")
     try:
